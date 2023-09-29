54fc341260cc4d2cad8f1b06a225fee70f1326ea
Bernhard Kauer <bk@alpico.io>
2023-09-29 11:34:04 +0200

    ap-auth-verify: switch to generic error codes instead of strings as they are easier to match against

--- rust/ap-auth-verify/src/lib.rs
+++ rust/ap-auth-verify/src/lib.rs
@@ -6,20 +6,28 @@ use ed25519_dalek::{Signature, VerifyingKey};
 pub mod header;
 
 #[derive(Debug)]
-pub enum Error {
+pub enum Error<E> {
     Check,
     Header(&'static str),
-    Key(&'static str),
+    Key(E),
+    KeyValue,
     Signature(&'static str),
     Time,
 }
 
 /// Verify a request.
-pub fn verify<'a, F, G>(method: &str, path: &'a str, body: &[u8], now: u64, get_header: F, get_key: G) -> Result<(), Error>
-where F: Fn(&str) -> Option<&'a str>,
-    G: FnOnce(u32) -> Result<[u8; 32], &'static str>,
+pub fn verify<'a, F, G, E>(
+    method: &str,
+    path: &'a str,
+    body: &[u8],
+    now: u64,
+    get_header: F,
+    get_key: G,
+) -> Result<(), Error<E>>
+where
+    F: Fn(&str) -> Option<&'a str>,
+    G: FnOnce(u32) -> Result<[u8; 32], E>,
 {
-
     // get the authorization header
     let raw_header = get_header("authorization").unwrap_or("");
     let header = header::AuthHeader::new(raw_header).map_err(Error::Header)?;
@@ -31,14 +39,13 @@ where F: Fn(&str) -> Option<&'a str>,
 
     // get the verifying key
     let pubkey = get_key(header.key).map_err(Error::Key)?;
-    let verifykey = VerifyingKey::from_bytes(&pubkey).or(Err(Error::Key("value")))?;
+    let verifykey = VerifyingKey::from_bytes(&pubkey).or(Err(Error::KeyValue))?;
 
     // decode the signature
     let signature_vec = general_purpose::URL_SAFE_NO_PAD
         .decode(header.sig)
         .or(Err(Error::Signature("base64")))?;
     let sig = Signature::from_slice(&signature_vec).or(Err(Error::Signature("len")))?;
-
 
     // produce the message to verify
     let mut message: Vec<u8> = Vec::new();
@@ -60,7 +67,6 @@ where F: Fn(&str) -> Option<&'a str>,
     }
     // always add the body
     message.extend(body);
-
 
     // verify it
     verifykey
